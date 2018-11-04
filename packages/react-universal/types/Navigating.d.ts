734d37712c1a1f396ca8c12681395f29ac337ef9
Paul Sherman <paul.sherman.88@gmail.com>
2018-11-04 12:39:42 -0600

    useNavigating()

--- packages/react-universal/types/Navigating.d.ts
+++ packages/react-universal/types/Navigating.d.ts
@@ -3,5 +3,4 @@ import { CancelActiveNavigation } from "@curi/router";
 export interface NavigatingProps {
     children(cancel: CancelActiveNavigation | void): ReactNode;
 }
-declare const Navigating: (props: NavigatingProps) => JSX.Element;
-export default Navigating;
+export default function Navigating(props: NavigatingProps): ReactNode;
